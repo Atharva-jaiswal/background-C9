4f45ead0616021939e23ef42ce96771270e77377
Atharva-jaiswal <89203446+Atharva-jaiswal@users.noreply.github.com>
2021-09-08 19:35:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background('yellow');
+  if (keyIsDown(RIGHT_ARROW))
+  {
+    background('green')
+  }
+  if (keyIsDown(LEFT_ARROW))
+  {
+    background('red');
+  }
+  if (keyIsDown(UP_ARROW))
+  {
+    background('purple');
+  }
+  if (keyIsDown(DOWN_ARROW))
+  {
+    background('blue');
+  }
+  drawSprites()
+}
+
+
+
+
+
+
